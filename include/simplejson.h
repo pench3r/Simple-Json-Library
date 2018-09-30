e92815b3861e82575a45d95c0403b16f1a73e2c7
pench3r <nageshangzei@gmail.com>
2018-09-30 21:57:05 +0800

    add some enum type

--- include/simplejson.h
+++ include/simplejson.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+// parse resutl type
 typedef enum {
 	SIMPLEJ_PARSE_OK = 0,
 	SIMPLEJ_PARSE_EXPECT_VALUE,
@@ -10,8 +11,37 @@ typedef enum {
 	SIMPLEJ_PARSE_ROOT_NOT_SINGULAR
 } SIMPLEJ_PARSE_RESULT;
 
-const char *sj_parse_str[4];
+// type
+typedef enum {
+	SIMPLEJ_NULL = 0,
+	SIMPLEJ_FALSE,
+	SIMPLEJ_TRUE,
+	SIMPLEJ_NUMBER,
+	SIMPLEJ_STRING,
+	SIMPLEJ_ARRAY,
+	SIMPLEJ_OBJECT
+} SIMPLEJ_TYPE;
 
-SIMPLEJ_PARSE_RESULT simplejson_parse(const char *str);
+// basic parse node struct
+typedef struct {
+	SIMPLEJ_TYPE sj_type;
+} SIMPLEJ_VALUE;
+
+const char *sj_parse_str[4];
+const char *sj_type_str[7];
+
+/* parse function
+param: SIMPLEJ_VALUE to recv parse value type
+			 str that present input string
+return: parse result type			 
+*/
+SIMPLEJ_PARSE_RESULT simplejson_parse(SIMPLEJ_VALUE *sj_value, const char *str);
+
+/* get value type */
+SIMPLEJ_TYPE get_simplejson_type(const SIMPLEJ_VALUE *sj_value);
+
+SIMPLEJ_PARSE_RESULT simplejson_parse_value(SIMPLEJ_VALUE *sj_value, const char *str);
+
+SIMPLEJ_PARSE_RESULT simple_parse_null(SIMPLEJ_VALUE *sj_value, const char *str);
 
 #endif /* SIMPLEJSON_H__ */
--- src/simplejson.c
+++ src/simplejson.c
@@ -5,8 +5,33 @@ const char *sj_parse_str[4] = {
   "SIMPLEJ_PARSE_INVALID_VALUE", "SIMPLEJ_PARSE_ROOT_NOT_SINGULAR"
 };
 
+const char *sj_type_str[7] = {
+	"SIMPLEJ_NULL", "SIMPLEJ_FALSE", "SIMPLEJ_TRUE",
+	"SIMPLEJ_NUMBER", "SIMPLEJ_STRING", "SIMPLEJ_ARRAY",
+	"SIMPLEJ_OBJECT"
+}
 
-SIMPLEJ_PARSE_RESULT simplejson_parse(const char *input_str) {
-	printf("%s: parse ok\n", input_str);
-	return SIMPLEJ_PARSE_OK;
+SIMPLEJ_TYPE get_simplejson_type(const SIMPLEJ_VALUE *sj_value) {
+	return sj_value->sj_type;
+}
+
+SIMPLEJ_PARSE_RESULT simple_parse_null(SIMPLEJ_VALUE *sj_value, const char *str) {
+	char *tmp_str = str;
+	tmp_str++;
+	if (tmp_str[0] == 'u' && tmp_str[1] == 'l' && tmp_str[2] == 'l' && tmp_str[3] == '\0')
+		sj_value->sj_type = SIMPLEJ_NULL;
+		return SIMPLEJ_PARSE_OK;	
+	return SIMPLEJ_PARSE_INVALID_VALUE;
+}
+
+SIMPLEJ_PARSE_RESULT simplejson_parse_value(SIMPLEJ_VALUE *sj_value, const char *str) {
+	char first_char = *str;
+	switch(first_char) {
+		case 'n': simplejson_parse_null(sj_value, str);
+		default: return SIMPLEJ_PARSE_INVALID_VALUE;
+	}	
+}
+
+SIMPLEJ_PARSE_RESULT simplejson_parse(SIMPLEJ_VALUE *sj_value, const char *input_str) {
+	return simplejson_parse_value(&sj_value, input_str);
 }
